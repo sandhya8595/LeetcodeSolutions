17d3429c73065b9242d1a876062fb5f830334c3b
sandhya8595 <isandhya891@gmail.com>
2026-03-24 14:29:40 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.cpp
@@ -0,0 +1,16 @@
+// Last updated: 24/03/2026, 14:29:39
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>9){
+     int ans=0,rem;
+     while(num!=0){
+        rem = num%10;
+        num = num/10;
+        ans = ans+rem;
+     }  
+    num =ans;
+        }
+    return num;
+    }
+};
