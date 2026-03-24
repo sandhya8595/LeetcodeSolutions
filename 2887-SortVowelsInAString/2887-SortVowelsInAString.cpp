905e126fd03a9e746de87c987f0b7e8208838102
sandhya8595 <isandhya891@gmail.com>
2026-03-24 14:28:29 +0530

    Create 2887-SortVowelsInAString

---
+++ 2887-SortVowelsInAString/2887-SortVowelsInAString.cpp
@@ -0,0 +1,50 @@
+// Last updated: 24/03/2026, 14:28:28
+class Solution {
+public:
+    string sortVowels(string s) {
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        for(int i =0;i<s.size();i++){
+            //lower a,e,i,o,u
+           
+        if(s[i]=='a'|| s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u'){
+            lower[s[i]-'a']++;
+            s[i]='#';
+        }
+        //upperA,E,I,O,U
+        else if(s[i]=='A'|| s[i]=='E'|| s[i]=='I'|| s[i]=='O'|| s[i]=='U')
+        {
+            upper[s[i]-'A']++;
+            s[i]='#';
+        }
+        }
+        //now sort the uper and lower case string
+        string vowel;
+        for(int i =0;i<26;i++){
+            char c = 'A'+i;
+            while(upper[i])
+            {
+             vowel+=c;
+             upper[i]--;
+           }
+        }
+        for(int i =0;i<26;i++){
+            char c = 'a'+i;
+            while(lower[i])
+            {
+             vowel+=c;
+             lower[i]--;
+            } 
+        }
+        int first= 0,second=0;//secondvowel ko index ko point kr rha hai 
+        while(second<vowel.size())
+        {
+            if(s[first]=='#'){
+                s[first]=vowel[second];
+                second++;
+            }
+            first++;
+        }
+        return s ;
+    }
+};
