64f6815387f3b0a082966c144721765da5a5d39d
sandhya8595 <isandhya891@gmail.com>
2026-03-24 14:29:44 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.cpp
@@ -0,0 +1,63 @@
+// Last updated: 24/03/2026, 14:29:43
+
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+ class MyQueue {
+  public:
+    stack<int>s1;//push
+    stack<int>s2; //stack for pop  
+ MyQueue(){}
+
+ bool empty(){
+ return s1.empty() && s2.empty();
+ }
+
+ void push(int x){// stack 1
+    s1.push(x);
+ }
+ int pop(){ 
+    //empty 
+   if(empty())
+   return 0;
+   //check stack 2 is empty or not
+   else if( !s2.empty())
+   {
+    int element = s2.top();
+    s2.pop();
+    return element;
+   }
+  else
+  {
+    while(!s1.empty())
+    {
+        s2.push(s1.top());
+        s1.pop();
+     }
+      int element = s2.top();
+      s2.pop();
+      return element;
+     }
+   }
+  int peek(){
+  if(empty())
+      return 0;
+   else if(!s2.empty()){
+    return s2.top();
+   }
+   else{
+     while(!s1.empty())
+     {
+        s2.push(s1.top());
+        s1.pop();
+     }
+     return s2.top();
+   }
+   }
+};
