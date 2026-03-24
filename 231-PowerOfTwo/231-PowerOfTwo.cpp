986de4123351074892b76c04c2a9bbcdb956d67d
sandhya8595 <isandhya891@gmail.com>
2026-03-24 14:29:45 +0530

    Create 231-PowerOfTwo

---
+++ 231-PowerOfTwo/231-PowerOfTwo.cpp
@@ -0,0 +1,14 @@
+// Last updated: 24/03/2026, 14:29:44
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<1)
+        return 0;
+       while(n!=1){
+if(n%2==1)
+return 0;
+n=n/2;
+       }
+     return 1;
+    }
+};
