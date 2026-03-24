d814f70749f54d59f3183af8c34342080745d512
sandhya8595 <isandhya891@gmail.com>
2026-03-24 14:31:24 +0530

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -0,0 +1,42 @@
+// Last updated: 24/03/2026, 14:31:23
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        ListNode* dummy = new ListNode(0);
+         ListNode* curr = dummy;
+        int carry = 0; 
+
+        while(l1!=NULL || l2!=NULL || carry !=0){
+           int  sum = carry;
+
+           if(l1!=NULL){
+            sum+=l1->val;
+            l1= l1->next;
+           }
+
+           if(l2!=NULL){
+            sum+=l2->val;
+            l2=l2->next;
+           }
+
+             carry = sum/10;
+            int  digit=sum%10;
+           curr->next= new ListNode(digit);
+           curr=curr->next;
+    } 
+    return dummy->next;
+    }
+};
+// we add on the number from the  right side just because of to handle the carry.... dn carry move from r to l(so reverse in manner)
