8f12a5ae001ecd75f1363378f63f2a53c58311f3
sandhya8595 <isandhya891@gmail.com>
2026-04-26 14:42:20 +0530

    Create 200-NumberOfIslands

---
+++ 200-NumberOfIslands/200-NumberOfIslands.cpp
@@ -0,0 +1,31 @@
+// Last updated: 26/04/2026, 14:42:20
+class Solution {
+public:
+  void dfs(int i ,int j,vector<vector<bool>>& vis,vector<vector<char>>& grid  ,int n ,int m){
+    if(i<0 || j<0 || i>=n ||j>=m || vis[i][j] || grid[i][j] !='1' ) return ;
+
+    vis[i][j]=true;
+     dfs(i-1,j, vis ,grid,n,m); //top
+     dfs(i,j+1,vis,grid,n,m); // right
+     dfs(i+1,j,vis,grid,n,m); // bottom
+     dfs(i,j-1,vis,grid,n,m); // left
+ }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
+        int n= grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>> vis(n,vector<bool>(m,false) );
+
+         for(int i =0;i<n;i++){
+            for(int j=0; j<m;j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                dfs(i,j,vis,grid ,n,m); // ek component ko vist
+              islands++;
+            }
+         }
+         }
+          return islands;
+    }
+};
