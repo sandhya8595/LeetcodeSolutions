34c07f818ebd82210527f1a26260d2abfb36bc38
sandhya8595 <isandhya891@gmail.com>
2026-06-16 23:20:53 +0530

    Create 199-BinaryTreeRightSideView

---
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.cpp
@@ -0,0 +1,46 @@
+// Last updated: 16/06/2026, 23:20:54
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+    class Solution {
+    public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+
+        if (!root) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+//level ka last node ke liye..
+                if (i == n - 1)
+                    ans.push_back(temp->val);
+
+                if (temp->left)
+                    q.push(temp->left);
+
+                if (temp->right)
+                    q.push(temp->right);
+            }
+        }
+
+        return ans;
+    }
+};
+
+// solve it also dfs(by using recursion)
