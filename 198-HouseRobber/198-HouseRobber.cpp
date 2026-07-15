21d10ad5895e30861c9c5f39a11f82a92b1be046
Sandhya Rajput <isandhya891@gmail.com>
2026-07-15 17:21:21 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -0,0 +1,21 @@
+// Last updated: 15/07/2026, 17:21:24
+class Solution {
+public:
+//using tabulation method
+    int rob(vector<int>& nums) {
+        int  n = nums.size();
+      
+      if(n==1) return nums[0];
+        vector<int>dp(n);
+
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+
+       for(int i=2;i<n;i++){
+        dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+       }
+       return dp[n-1];
+  
+    }
+};
