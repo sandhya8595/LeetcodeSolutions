6724eb3dfbd5ff888f9955c2932b08bbdacb281b
Sandhya Rajput <isandhya891@gmail.com>
2026-07-16 10:47:26 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.cpp
@@ -0,0 +1,19 @@
+// Last updated: 16/07/2026, 10:47:30
+class Solution {
+public:
+//using tabulation method
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+
+        vector<int> dp(n);
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        }
+
+        return min(dp[n - 1], dp[n - 2]);
+    }
+};
