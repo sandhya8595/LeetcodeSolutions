faa29c90dc090456db3c16682b4d6f5d3c1118af
Sandhya Rajput <isandhya891@gmail.com>
2026-07-17 22:12:30 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,19 @@
+// Last updated: 17/07/2026, 22:12:29
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+                if (i > maxReach)
+                return false;
+
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= nums.size() - 1)
+                return true;
+        }
+
+        return true;
+    }
+};
